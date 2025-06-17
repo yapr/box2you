49fc941beb824bcb5dcf42a9ad641a7ad5c1b5cc
Jun Takeno <take.yapr@gmail.com>
2025-06-17 14:06:07 +0900

    yet

--- app/models/book_translation.rb
+++ app/models/book_translation.rb
@@ -1,12 +1,12 @@
 class BookTranslation < ApplicationRecord
   include PgSearch::Model
-  
+
   belongs_to :book
-  
+
   validates :title, presence: true
   validates :language, presence: true
   validates :book_id, uniqueness: { scope: :language }
-  
+
   pg_search_scope :search_by_title,
     against: :title,
     using: {
@@ -16,6 +16,6 @@ class BookTranslation < ApplicationRecord
         dictionary: "english"
       }
     }
-  
+
   scope :in_language, ->(lang) { where(language: lang) }
 end
